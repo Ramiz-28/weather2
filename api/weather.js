d06070a2da250886c183616565c1191750431f4b
Ramiz-28 <ramizshakil28@gmail.com>
2026-05-13 12:38:49 +0500

    update api/js

--- api/weather.js
+++ api/weather.js
@@ -1,22 +1,13 @@
 export default async function handler(req, res) {
-  const { city } = req.query;
+  const { lat, lon } = req.query;
 
-  const API_KEY = process.env.WEATHER_KEY;
+  const apiKey = process.env.WEATHER_API_KEY;
 
-  try {
-    const weatherRes = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-    );
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+  );
 
-    const forecastRes = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
-    );
+  const data = await response.json();
 
-    const weather = await weatherRes.json();
-    const forecast = await forecastRes.json();
-
-    res.status(200).json({ weather, forecast });
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch weather" });
-  }
+  res.status(200).json(data);
 }
