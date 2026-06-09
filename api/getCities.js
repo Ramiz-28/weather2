ff073a91c527b49948f8012c1ec5976ebe9393d0
Ramiz-28 <ramizshakil28@gmail.com>
2026-06-09 12:47:30 +0500

    debug logs

--- api/getCities.js
+++ api/getCities.js
@@ -2,14 +2,18 @@ import clientPromise from "/lib/mongodb";
 
 export default async function handler(req, res) {
   try {
+    console.log("API HIT"); // 👈 ADD THIS
+
     const client = await clientPromise;
+    console.log("Mongo connected"); // 👈 ADD THIS
+
     const db = client.db("weatherDB");
 
     const cities = await db.collection("cities").find({}).toArray();
 
     res.status(200).json(cities);
   } catch (error) {
-    console.log("GET CITIES ERROR:", error);
+    console.log("GET CITIES ERROR:", error); // 👈 IMPORTANT
     res.status(500).json({ error: "Server error" });
   }
 }
