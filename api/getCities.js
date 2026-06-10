e4e6013aa76290aeb2f79abb2da43b188d12f50e
Ramiz-28 <ramizshakil28@gmail.com>
2026-06-10 21:10:48 +0500

    update getCities.js

--- api/getCities.js
+++ api/getCities.js
@@ -1,15 +1,10 @@
-import clientPromise from "../lib/mongodb";
-
 export default async function handler(req, res) {
   try {
-    const client = await clientPromise;
-    const db = client.db("weatherDB");
-
-    const cities = await db.collection("cities").find({}).toArray();
-
-    res.status(200).json(cities);
+    return res.status(200).json({
+      message: "API working",
+      env: process.env.MONGO_URI ? "ENV FOUND ✅" : "ENV MISSING ❌"
+    });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
+    return res.status(500).json({ error: error.message });
   }
 }
