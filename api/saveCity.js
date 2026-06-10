39eeea00a16b78b5f65098afc78e39f31b7836fb
Ramiz-28 <ramizshakil28@gmail.com>
2026-06-10 12:03:31 +0500

    show real error

--- api/saveCity.js
+++ api/saveCity.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
       return res.status(200).json({ success: true });
     } catch (error) {
       console.log("SAVE CITY ERROR:", error);
-      return res.status(500).json({ error: "Server error" });
+      res.status(500).json({ error: error.message });
     }
   }
 
