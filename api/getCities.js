4a66137d78acce54f0da0c9b6c2276e7835cc5e6
Ramiz-28 <ramizshakil28@gmail.com>
2026-06-10 17:23:38 +0500

    upadte api

--- api/getCities.js
+++ api/getCities.js
@@ -1,10 +1,15 @@
+import clientPromise from "../lib/mongodb";
+
 export default async function handler(req, res) {
   try {
-    return res.status(200).json({
-      message: "API working",
-      env: process.env.MONGO_URI ? "ENV FOUND ✅" : "ENV MISSING ❌"
-    });
+    const client = await clientPromise;
+    const db = client.db("weatherDB");
+
+    const cities = await db.collection("cities").find({}).toArray();
+
+    res.status(200).json(cities);
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    console.log(error);
+    res.status(500).json({ error: error.message });
   }
 }
