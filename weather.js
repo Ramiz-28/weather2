5b852542657b9791c9dbfdf4b82c09fc88879bb2
Ramiz-28 <ramizshakil28@gmail.com>
2026-06-15 20:37:58 +0500

    update html and js

--- weather.js
+++ weather.js
@@ -1,3 +1,37 @@
+const SUPABASE_URL = "https://llcagxshbflnepqrusgf.supabase.co";
+const SUPABASE_ANON_KEY = "YOUR_ANON_KEY_HERE";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
+
+async function signUp() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  let { error } = await supabase.auth.signUp({ email, password });
+
+  if (error) alert(error.message);
+  else alert("Signup successful!");
+}
+
+async function login() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  let { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) alert(error.message);
+  else {
+    alert("Login successful!");
+    renderCities(); // reload cities
+  }
+}
+
 
 let btn = document.getElementById("getWeather");
 
